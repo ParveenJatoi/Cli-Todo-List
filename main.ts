03c1b666b05ccf9bdc9faf89f437169c0f4ba9b9
Parveen Jatoi <pjatoi973@gmail.com>
2024-04-11 04:42:37 -0700

    first todo list

---
+++ main.ts
@@ -0,0 +1,44 @@
+#!/usr/bin/env node 
+
+import inquirer from "inquirer";
+let todos :string[]= [];
+let condition =true;
+
+while ( condition){ 
+ 
+let addTask = await inquirer.prompt([ 
+    {
+      name:'todo',
+
+       type:'input',
+      message:"what you want to add in your Todos"
+},
+{
+    name:'addMore',
+    type:'confirm',
+    message:"Do you want to add more?",
+
+    default:"false"
+},
+ ]
+)
+todos.push(addTask.todo);
+condition=addTask.addMore
+console.log(todos);
+}
+
+
+
+
+            
+
+
+        
+       
+
+
+// todos.push(addTask.list);
+// b = addTask.addMore
+// console.log(`your Todos :${todos}`);
+// }
+
